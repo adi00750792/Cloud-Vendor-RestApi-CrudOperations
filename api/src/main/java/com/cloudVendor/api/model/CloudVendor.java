74215975de137a21148909169d8d733d7534ff61
Aditya <aditya.gautam2001@gmail.com>
2023-08-20 13:50:33 +0530

    initial project commit

---
+++ api/src/main/java/com/cloudVendor/api/model/CloudVendor.java
@@ -0,0 +1,39 @@
+package com.cloudVendor.api.model;
+
+public class CloudVendor {
+    private int vendorId;
+    private String vendorName;
+    private String vendorAddress;
+    private int phoneNumber;
+    public int getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(int vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getVendorAddress() {
+        return vendorAddress;
+    }
+
+    public void setVendorAddress(String vendorAddress) {
+        this.vendorAddress = vendorAddress;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ api/src/test/java/com/cloudVendor/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudVendor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
